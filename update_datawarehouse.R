62f98e07e49be1f9cefd74c2f4cde2c270e15876
Tom Jemmett <tom.jemmett@gmail.com>
2021-07-14 14:23:16 +0100

    replaces hello world script with update dw script

---
+++ update_datawarehouse.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+library(lubridate)
+library(DBI)
+library(NHSRdatasets)
+
+my_dw <- "c:\\my_datawarehouse.db"
+con <- dbConnect(SQLite(), my_dw)
+
+on.exit(dbDisconnect(con))
+
+ae_df <- filter(ae_attendances, period >= ymd(20180401))
+dbWriteTable(con, "ae_attendances", ae_df, append = TRUE)
+
+n <- tbl(con, "ae_attendances") |> count()
+
+cat("The table now has", n, "rows\n")
